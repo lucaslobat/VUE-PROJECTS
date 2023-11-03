d84b95bb258a0989f2364d7888ea6b7f8ecc2d5e
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-11-03 20:08:48 +0100

    Created the CoachCard and CoachList components. CoachList receive the getter getAllCoaches from the store and iterate over the state elements to render multiple CoachCard's passing data via props

--- find-a-coach/vite.config.js
+++ find-a-coach/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => ["md-linedivider"].includes(tag),
-        },
-      },
-    }),
-  ],
-});
+  plugins: [vue()],
+})
