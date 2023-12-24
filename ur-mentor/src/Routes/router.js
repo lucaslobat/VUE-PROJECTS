dbd473f78a893ab8eee50b95e81560be2d552544
Lucas Lobato <criptoryu@gmail.com>
2023-12-24 13:00:05 +0100

    Separated the main views from its subcomponents for better organization

--- ur-mentor/src/Routes/router.js
+++ ur-mentor/src/Routes/router.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import MentorList from "../components/MentorComponents/MentorList.vue";
+import MentorList from "../Views/MentorList.vue";
 import MentorDetails from "../components/MentorComponents/MentorDetails.vue";
 import MentorContactForm from "../components/MentorComponents/MentorContactForm.vue";
-import RequestList from "../components/RequestComponents/RequestList.vue";
+import RequestList from "../Views/RequestList.vue";
 import MentorRegistration from "../components/MentorComponents/MentorRegistration.vue";
 
 const router = createRouter({
