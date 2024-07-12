d4ea30db614007571e418f9478414f837d541874
Lucas Lobato <90288775+lucaslobat@users.noreply.github.com>
2024-07-12 21:41:21 +0200

    Delete scholar-docs directory

--- scholar-docs/client/src/plugins/serpapi.js
+++
@@ -1,6 +0,0 @@
-import { config, getJson } from "serpapi";
-
-config.api_key = "5572b53c08aefc7afb90329d683c97766daa7b5ba794571918e807fa0f93494c";
-config.timeout = 60000;
-
-console.log(await getJson({ engine: "google", q: "coffee" })); // uses the API key defined in the config
